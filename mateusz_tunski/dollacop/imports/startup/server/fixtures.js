9c743587de65255aaa8aad982a00c9ec374dc0c6
Mateusz Tuński <mateusz@tunski.co>
2016-06-07 13:44:39 +0200

    Add some fixtures

---
+++ mateusz_tunski/dollacop/imports/startup/server/fixtures.js
@@ -0,0 +1,68 @@
+import { Meteor } from "meteor/meteor"
+
+import { Debts } from "/imports/api/Debts"
+
+let index = 1
+do {
+  if (!Accounts.findUserByEmail(`u${index}@t.com`)) {
+    const id = Accounts.createUser({
+      email: `u${index}@t.com`,
+      password: "password",
+      profile: { name: `u${index}` }
+    })
+  }
+  index++
+} while(index <= 5)
+
+const users = Meteor.users.find({}).fetch()
+
+if (Debts.find().count() === 0) {
+  Debts.insert({
+    creditor: { _id: users[0]._id, name: users[0].profile.name },
+    debtors: [
+      { _id: users[1]._id, name: users[1].profile.name },
+      { _id: users[2]._id, name: users[2].profile.name }
+    ],
+    items: [
+      { name: "testitem1", price: 100 },
+      { name: "testitem2", price: 150 },
+      { name: "testitem3", price: 200 }
+    ],
+    settled: false,
+    createdAt: Date.now()
+  })
+
+  Debts.insert({
+    creditor: { _id: users[0]._id, name: users[0].profile.name },
+    debtors: [
+      { _id: users[1]._id, name: users[1].profile.name }
+    ],
+    items: [ {name: "testitem4", price: 250 }],
+    settled: false,
+    createdAt: Date.now()
+  })
+
+  Debts.insert({
+    creditor: { _id: users[0]._id, name: users[0].profile.name },
+    debtors: [
+      { _id: users[2]._id, name: users[2].profile.name }
+    ],
+    items: [{ name: "testitem1", price: 100 }],
+    settled: false,
+    createdAt: Date.now()
+
+  })
+
+  Debts.insert({
+    creditor: { _id: users[1]._id, name: users[1].profile.name },
+    debtors: [
+      { _id: users[0]._id, name: users[0].profile.name }
+    ],
+    items: [
+      { name: "testitem1", price: 100 },
+      { name: "testitem2", price: 150 }
+    ],
+    settled: false,
+    createdAt: Date.now()
+  })
+}
